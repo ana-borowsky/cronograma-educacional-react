2a5ea0ee680b5d682fd014ebaca9b2d981637e5c
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-10-13 11:06:13 -0300

    Adiciona barra de progresso na home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -46,7 +46,20 @@ const Home = () => {
             </button>
           </div>
 
+          <div className="daily-progress">
+            <div className="progress-bar-container">
+              <div className="progress-bar" style={{ width: '40%' }}></div>
+            </div>
+            <p className="progress-info">4 de 10 Horas Concluídas</p>
+          </div>
+
           <div className="todo-list">
+
+            <div className="todo-item">
+              <span className="item-time">08:00 - 09:00</span>
+              <span className="item-description">Preparação para a aula de Programação</span>
+              <input type="checkbox" className="item-checkbox" />
+            </div>
 
             <div className="todo-item">
               <span className="item-time">09:00 - 10:00</span>
@@ -54,10 +67,22 @@ const Home = () => {
               <input type="checkbox" className="item-checkbox" />
             </div>
 
+            <div className="todo-item completed">
+              <span className="item-time">10:00 - 12:00</span>
+              <span className="item-description">Estudar para a prova de Física Experimental</span>
+              <input type="checkbox" className="item-checkbox" checked readOnly />
+            </div>
+
             <div className="todo-item">
               <span className="item-time">13:00 - 15:00</span>
               <span className="item-description">Reunião de grupo para Projeto Final</span>
               <input type="checkbox" className="item-checkbox" />
+            </div>
+
+            <div className="todo-item completed">
+              <span className="item-time">15:00 - 17:00</span>
+              <span className="item-description">Redigir a primeira seção do relatório de Cálculo</span>
+              <input type="checkbox" className="item-checkbox" checked readOnly />
             </div>
 
             <div className="todo-item completed">
@@ -69,6 +94,12 @@ const Home = () => {
             <div className="todo-item">
               <span className="item-time">19:00 - 21:00</span>
               <span className="item-description">Aula de Introdução à Programação (Lab Inf 3)</span>
+              <input type="checkbox" className="item-checkbox" />
+            </div>
+
+            <div className="todo-item">
+              <span className="item-time">21:00 - 22:00</span>
+              <span className="item-description">Planejar tarefas para o dia seguinte</span>
               <input type="checkbox" className="item-checkbox" />
             </div>
 

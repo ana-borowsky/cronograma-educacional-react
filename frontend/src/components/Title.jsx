e188a8970534cffe0ed1afcd7e46139a0d22ad8c
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-10-21 12:23:05 -0300

    Adiciona margem top ao title

--- frontend/src/components/Title.jsx
+++ frontend/src/components/Title.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Title = ({ children }) => {
   return (
     <Link to="/">
-        <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight mb-8 leading-tight 
+        <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight mb-8 mt-20 leading-tight 
                       bg-clip-text text-transparent bg-gradient-to-r from-yellow-300 to-orange-500 
                       drop-shadow-lg transition-all duration-300 hover:scale-[1.01]">
           {children}

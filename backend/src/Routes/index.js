22ecde9007b07939fd4754c61d75f978d955c177
Reshi <duuhmorciani@gmail.com>
2025-10-27 16:18:03 -0300

    Faz a integração com o Gemini, cria o controller gemini.js, cria a rota para que o Gemini processar o PDF.

--- backend/src/Routes/index.js
+++ backend/src/Routes/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import { getScheduleEvents } from "../Controllers/ScheduleController.js";
 
 const router = express.Router()
-
 router.get("/ScheduleEvents", getScheduleEvents)
 console.log("Backend rodando 😎")
 

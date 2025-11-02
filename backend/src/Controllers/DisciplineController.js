c4c6504ae716b3a5a6ae500794c0534279608f3e
gustamara <gustacamra.dev@hotmail.com>
2025-11-02 05:18:02 -0300

    Implementa funcionalidade de delete e getAll; Refatora codigo para identaçao de 2 tabs; Remove pontos e virgulas do codigo;

--- backend/src/Controllers/DisciplineController.js
+++ backend/src/Controllers/DisciplineController.js
@@ -1,31 +1,64 @@
-import DisciplineModel from "../Models/DisciplineModel.js";
+import DisciplineModel from "../Models/DisciplineModel.js"
 
 class DisciplineController {
-   static async insertDiscipline(req, res) {
+  static async insertDiscipline(req, res) {
     try {
       const { name, color, project, classroom, day, startTime, endTime, weight, idUser } = req.body
 
-      const discipline = new DisciplineModel(name, color, project, classroom, day, startTime, endTime, weight, idUser);
+      const discipline = new DisciplineModel(name, color, project, classroom, day, startTime, endTime, weight, idUser)
       const result = await discipline.insert()
 
-      res.status(200).json({ id: result.insertId, name });
+      res.status(200).json({ id: result.insertId, name })
     } catch (err) {
       console.error(err)
       res.status(500).json({ error: "Erro ao inserir a disciplina" })
     }
   }
 
-  static async updateDiscipline(req , res) {
+  static async delete(req, res) {
     try {
-        const {idDiscipline, name, color, project, classroom, day, startTime, endTime, weight} = req.body
+      const { idDiscipline } = req.body
+      const discipline = new DisciplineModel({ ...req.body })
+      const deleteResult = await discipline.delete(idDiscipline)
+      if (deleteResult.affectedRows === 0) {
+        return res.status(404).json({ error: "Disciplina não encontrada" })
+      }
 
-        const discipline = new DisciplineModel(name, color, project, classroom, day, startTime, endTime, weight)
-        const updateResult = await discipline.update(idDiscipline)
-
-        res.status(200).json( {idDiscipline, name, color, project, classroom, day, startTime, endTime, weight })
+      return res.status(200).json({ ...req.body })
     } catch (err) {
-        console.error(err)
-        res.status(500).json({ error: "Erro ao atualizar a disciplina"})
+      console.log(err)
+      res.status(400).json({ err: "Erro ao deletar o disciplina." })
+    }
+  }
+
+  static async updateDiscipline(req, res) {
+    try {
+      const { idDiscipline, name, color, project, classroom, day, startTime, endTime, weight } = req.body
+
+      const discipline = new DisciplineModel(name, color, project, classroom, day, startTime, endTime, weight)
+      const updateResult = await discipline.update(idDiscipline)
+
+      res.status(200).json({ idDiscipline, name, color, project, classroom, day, startTime, endTime, weight })
+    } catch (err) {
+      console.error(err)
+      res.status(500).json({ error: "Erro ao atualizar a disciplina" })
+    }
+  }
+
+  static async getAll(req, res) {
+    try {
+      const { idUser } = req.body
+      const discipline = new DisciplineModel({ ...req.body })
+      const result = await discipline.getAll(idUser)
+
+      if (!result || (result.length === 0)) {
+        return res.status(404).json({ error: "Usuário não encontrado!" })
+      }
+
+      return res.status(200).json(result)
+    } catch (err) {
+      console.log(err)
+      res.status(500).json({ error: "Erro ao obter disciplinas" })
     }
   }
 }
--- backend/src/Models/DisciplineModel.js
+++ backend/src/Models/DisciplineModel.js
@@ -1,53 +1,67 @@
 import { db } from "../db.js";
 
 class DisciplineModel {
-    constructor(name, color, project, classroom, day, startTime, endTime, weight, idUser) {
-        this.name = name
-        this.color = color
-        this.project = project
-        this.classroom = classroom 
-        this.day = day
-        this.startTime = startTime
-        this.endTime = endTime
-        this.weight = weight
-        this.idUser = idUser
-    }
+  constructor(name, color, project, classroom, day, startTime, endTime, weight, idUser) {
+    this.name = name
+    this.color = color
+    this.project = project
+    this.classroom = classroom
+    this.day = day
+    this.startTime = startTime
+    this.endTime = endTime
+    this.weight = weight
+    this.idUser = idUser
+  }
 
-    async insert() {
-        const values = [
-            this.name,
-            this.color, 
-            this.project,
-            this.classroom,
-            this.day,
-            this.startTime,
-            this.endTime,
-            this.weight, 
-            this.idUser
-        ]
-        const query = "INSERT INTO discipline (name, color, project, classroom, day, startTime, endTime, weight, idUser) VALUES (?,?,?,?,?,?,?,?,?)"
-        const [result] = await db.query(query, values)
+  async insert() {
+    const values = [
+      this.name,
+      this.color,
+      this.project,
+      this.classroom,
+      this.day,
+      this.startTime,
+      this.endTime,
+      this.weight,
+      this.idUser
+    ]
+    const query = "INSERT INTO discipline (name, color, project, classroom, day, startTime, endTime, weight, idUser) VALUES (?,?,?,?,?,?,?,?,?)"
+    const [result] = await db.query(query, values)
 
-        return result
-    }
+    return result
+  }
 
-    async update(idDiscipline) {
-        const values = [
-            this.name,
-            this.color, 
-            this.project,
-            this.classroom,
-            this.day,
-            this.startTime,
-            this.endTime,
-            this.weight, 
-            idDiscipline
-        ]
-        const query = "UPDATE discipline SET name = ?, color = ?, project = ?, classroom = ?, day = ?, startTime = ?, endTime = ?, weight = ? WHERE idDiscipline = ?"
-        const result = await db.query(query, values)
+  async update(idDiscipline) {
+    const values = [
+      this.name,
+      this.color,
+      this.project,
+      this.classroom,
+      this.day,
+      this.startTime,
+      this.endTime,
+      this.weight,
+      idDiscipline
+    ]
+    const query = "UPDATE discipline SET name = ?, color = ?, project = ?, classroom = ?, day = ?, startTime = ?, endTime = ?, weight = ? WHERE idDiscipline = ?"
+    const result = await db.query(query, values)
 
-        return result
-    }
+    return result
+  }
+
+  async delete(idDiscipline) {
+    const values = [idDiscipline]
+    const query = "DELETE FROM discipline WHERE idDiscipline = ?"
+    const [result] = await db.query(query, values)
+    return result
+  }
+
+  async getAll(idUser) {
+    const values = [idUser]
+    const query = "SELECT (*) WHERE idUser = ?"
+    const [result] = await db.query(query, values)
+    return result
+  }
 }
 
 export default DisciplineModel

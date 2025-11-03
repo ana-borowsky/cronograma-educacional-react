f2598f9f81cc7948ad80f16088c61f9d89de1033
Reshi <duuhmorciani@gmail.com>
2025-11-03 01:26:52 -0300

    Implementa as funcionalidades de select, insert, update e delete para as tasks.

--- backend/src/Routes/index.js
+++ backend/src/Routes/index.js
@@ -1,11 +1,9 @@
 import express from "express";
 import { getScheduleEvents } from "../Controllers/ScheduleController.js";
-import { getTasks } from "../Controllers/TasksController.js";
 
 const router = express.Router()
 
 router.get("/ScheduleEvents", getScheduleEvents)
-router.get("/Tasks", getTasks)
 
 console.log("Backend rodando 😎")
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import disciplineRoutes from "./Routes/dicipline-router.js"
+import taskRoutes from "./Routes/task-router.js"
 import cors from "cors"
 
 const app = express()
@@ -8,6 +9,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/discipline", disciplineRoutes)
+app.use("/tasks", taskRoutes);
 
 
 app.listen(8800)

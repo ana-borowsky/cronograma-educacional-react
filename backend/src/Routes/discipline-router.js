f023b90e9fa37a0c891fefe5e3d9b49c11fc270b
gustamara <gustacamra.dev@hotmail.com>
2025-11-03 13:27:07 -0300

    remove testes duplicados; corrige imports em discipline-router

--- backend/src/Routes/discipline-router.js
+++ backend/src/Routes/discipline-router.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"
 import DisciplineController from "../Controllers/DisciplineController.js"
 
 const router = express.Router()
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import disciplineRoutes from "./Routes/dicipline-router.js"
+import disciplineRoutes from "./Routes/discipline-router.js"
 import cors from "cors"
 
 const app = express()

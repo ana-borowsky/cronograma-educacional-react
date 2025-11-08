aaae47faa9b91852a0a2f0672c6e308f8435c38c
gustacamara <gustacamara.dev@hotmail.com>
2025-11-08 17:59:33 -0300

    cria serviços para freeTime

---
+++ backend/src/Services/FreeTimeService.js
@@ -0,0 +1,27 @@
+import FreeTimeRepository from "../Models/Repositories/FreeTimeRepository.js"
+
+class FreeTimeService {
+  constructor() {}
+
+  async insert(freeTimeModel) {
+    const repo = new FreeTimeRepository()
+    return await repo.insert(freeTimeModel)
+  }
+
+  async delete(idTime) {
+    const repo = new FreeTimeRepository()
+    return await repo.delete(idTime)
+  }
+
+  async update(freeTimeModel) {
+    const repo = new FreeTimeRepository()
+    return await repo.update(freeTimeModel)
+  }
+
+  async getAll(idTime) {
+    const repo = new FreeTimeRepository()
+    return await repo.getAll(idTime)
+  }
+}
+
+export default FreeTimeService

16c8ceb50165801356b0ab8618de791a82bfdb98
gustacamara <gustacamara.dev@hotmail.com>
2025-11-08 19:01:25 -0300

    Refatora controlador de tarefas para usar o serviço de tarefas

---
+++ backend/src/Services/TaskService.js
@@ -0,0 +1,40 @@
+import TasksRepository from "../Models/Repositories/TasksRepository"
+
+class TasksService {
+  constructor() { }
+
+  async insert(taskModel) {
+    const repo = new TasksRepository()
+    return await repo.insert(taskModel)
+  }
+
+  async delete(idTask) {
+    const repo = new TasksRepository()
+    return await repo.delete(idTask)
+  }
+
+  async update(taskModel) {
+    const repo = new TasksRepository()
+    return await repo.update(taskModel)
+  }
+  async getAll(idDiscipline) {
+    const repo = new TasksRepository()
+    return await repo.getAll(idDiscipline)
+  }
+
+  async getExam(idDiscipline) {
+    const repo = new TasksRepository()
+    return await repo.getExam(idDiscipline)
+  }
+
+  async getWork(idDiscipline) {
+    const repo = new TasksRepository()
+    return await repo.getWork(idDiscipline)
+  }
+
+  async getDayTask(idDiscipline) {
+    const repo = new TasksRepository()
+    return await repo.getDayTask(idDiscipline)
+  }
+  
+} export default TasksService

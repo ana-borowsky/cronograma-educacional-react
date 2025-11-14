136ba7f3d4678742351ea96a88cbf6cf44db58e9
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-11-14 10:33:41 -0300

    Atualiza design da navbar, removendo a linha inferior da tab ativa

--- frontend/src/containers/NavigationBar.jsx
+++ frontend/src/containers/NavigationBar.jsx
@@ -11,26 +11,30 @@ const NavigationBar = ({ currentPath = "/disciplines" }) => {
       <a
         href={href}
         className={`
-      flex items-center justify-center gap-2 px-4 pt-3 pb-2 text-lg font-semibold
-      transition duration-200 ease-in-out cursor-pointer relative z-10
-      
-      ${isActive
-            ? 'text-tx border-t border-l border-r border-neutral-800 rounded-tl-lg rounded-tr-lg'
-            :'text-neutral-500 hover:text-white hover:bg-neutral-500/50 hover:rounded-tl-lg hover:rounded-tr-lg'
+          flex items-center justify-center gap-2 px-4 pt-3 pb-2 text-lg font-semibold
+          transition duration-200 ease-in-out cursor-pointer relative z-10
+          
+          ${isActive
+            ? 'text-tx border-t border-l border-r border-neutral-400 rounded-tl-lg rounded-tr-lg'
+            : 'text-neutral-500 hover:text-white hover:bg-neutral-500/50 hover:rounded-tl-lg hover:rounded-tr-lg'
           }
-      ${!isActive && 'border-b border-transparent'} 
-    `}
+          ${!isActive && 'border-b border-transparent'} 
+        `}
       >
         {icon}
         <span>{children}</span>
+        {isActive && (
+          <div
+            className="absolute bottom-[-2px] left-0 right-0 h-[2px] bg-background z-20"
+          />
+        )}
       </a>
     );
   };
 
   return (
     <div className="w-full min-w-[1674px] flex justify-center">
-
-      <div className="w-full border-b border-neutral-700 flex justify-center items-end">
+      <div className="w-full border-b border-neutral-400 flex justify-center items-end">
 
         <div className="flex flex-row gap-0">
           <NavTab href="/disciplines" icon={<MdDescription className="w-5 h-5" />}>

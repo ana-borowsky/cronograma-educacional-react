33ee1fcea10e442180ad87af4510e86b20be2e27
ana <114084376+ana-borowsky@users.noreply.github.com>
2025-11-15 21:32:56 -0300

    Troca a cor quando exxos acontecem na leitura da IA e o usuário precisa editar algum dado

--- frontend/src/components/discipline/DisciplineTitle.jsx
+++ frontend/src/components/discipline/DisciplineTitle.jsx
@@ -33,17 +33,46 @@ export function DisciplineTitle({ title, project, weight, color, classroom }) {
       </h2>
 
       <div className="mt-1 mb-2 flex items-center gap-3 text-sm text-neutral-500">
-
-        <span className="px-2 py-0.5 bg-neutral-200 rounded-md font-medium">
-          {project || "Edite o nome do projeto"}
+        <span
+          className={`
+            px-2 py-0.5 rounded-md font-medium
+            ${String(project || "Edite o nome do projeto")
+                      .toLowerCase()
+                      .includes("edite")
+                      ? "bg-yellow-600 text-white"
+                      : "bg-neutral-200"
+                    }
+          `}
+                >
+          {truncateText(project || "Edite o nome do projeto", 20)}
         </span>
 
-        <span className="px-2 py-0.5 bg-neutral-200 rounded-md font-medium">
-          Peso: {weight || "Edite o nível de dificuldade"}
+        <span
+          className={`
+            px-2 py-0.5 rounded-md font-medium
+            ${String(weight || "Edite o nível de dificuldade")
+                      .toLowerCase()
+                      .includes("edite")
+                      ? "bg-yellow-600 text-white"
+                      : "bg-neutral-200"
+                    }
+          `}
+                >
+          {truncateText(weight || "Edite o nível de dificuldade", 20)}
         </span>
 
-        <span className="px-2 py-0.5 bg-neutral-200 rounded-md font-medium">
-          {classroom || "Edite a sala"}
+        <span
+          className={`
+            px-2 py-0.5 rounded-md font-medium
+            ${String(classroom || "Edite a sala")
+                      .toLowerCase()
+                      .includes("edite")
+                      ? "bg-yellow-600 text-white"
+                      : "bg-neutral-200"
+                    }
+          `}
+                >
+          {truncateText(classroom || "Edite a sala", 20)}
         </span>
 
       </div>
--- frontend/src/pages/Disciplines.jsx
+++ frontend/src/pages/Disciplines.jsx
@@ -75,9 +75,9 @@ const Disciplines = () => {
         openAddDisciplineModal={openAddDisciplineModal}
       />
 
-      <div className="flex flex-col p-5 gap-6">
-        <div className="w-full flex flex-col md:flex-row md:space-x-8">
-          <div className="w-full flex overflow-x-auto space-x-6 pb-4">
+      <div className="flex flex-col p-5 gap-6 items-center">
+        <div className="w-4/5 flex flex-col md:flex-row md:space-x-8">
+          <div className="flex overflow-x-scroll space-x-6 pb-4">
             {isLoading ? (
               <p className="text-neutral-400">Carregando disciplinas...</p>
             ) : (
